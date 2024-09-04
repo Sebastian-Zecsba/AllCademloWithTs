303b7fd135d85b58ec471620878b64848876b156
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2024-09-04 11:52:33 -0500

    Cors added

--- 02-Asociaciones/src/server.ts
+++ 02-Asociaciones/src/server.ts
@@ -1,11 +1,13 @@
 import "./models"
-import express from 'express'
 import { conectDB } from './config/db'
+import { corsConfig } from "./config/cors"
+import express from 'express'
 import router from './routes'
+import cors from "cors"
 
 conectDB()
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/v1/', router)
