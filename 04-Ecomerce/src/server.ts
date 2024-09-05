bc50a69677bea1f8852cd8b626e9da077ed52427
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2024-09-05 18:29:54 -0500

    Starting with the ecomerce

---
+++ 04-Ecomerce/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import { conectDB } from './config/db'
+import cors from 'cors'
+import router from './routes'
+
+conectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', router)
+app.get('/', (req, res) => {
+    return res.send('Welcome to express')
+})
+
+export default app
---
+++ 04-Ecomerce/src/utils/verify.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+
+dotenv.config()
+
+declare global{ 
+    namespace Express{ 
+        interface Request {
+            user?: number
+        }
+    }
+}
+
+const verifyJwt = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization as string;
+    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err, decoded) => {
+        if (err) return res.sendStatus(403);
+
+        const decodedToken = decoded as { id: number };
+
+        if (!decodedToken.id) return res.sendStatus(403); 
+        
+        req.user = decodedToken.id;
+        next();
+    });
+}
+
+export default verifyJwt;
